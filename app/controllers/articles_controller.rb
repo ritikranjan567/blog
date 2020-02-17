e665f14ce2f3ceaa35187f4d2410e385107df291
ritikranjan567 <ranjanritik219@gmail.com>
2020-02-17 15:18:04 +0530

    added basic security

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController < ApplicationController
+    
+    http_basic_authenticate_with name: "ritik", password: "lab@1234", except: [:index, :show]
+
     def index
         @articles = Article.all
     end
